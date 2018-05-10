423ccf1a6ceb39351674a83d7075a9f28b16668b
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2018-05-10 14:21:46 -0700

    possibly this is pointless

--- keyboards/dz60/keymaps/mine/keymap.c
+++ keyboards/dz60/keymaps/mine/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------------+
  * | TG2    |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  |    Enter    |
  * |-----------------------------------------------------------------------------------------+
- * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | RSh |  U  | FN  |
+ * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |Bkspc|  U  | FN  |
  * |-----------------------------------------------------------------------------------------+
  * | Ctrl |  Alt  |  Cmd  |              Space                | Cmd | RAlt |  L  |  D  |  R  |
  * `-----------------------------------------------------------------------------------------'
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
       TG(2), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
-      KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_MS_WH_DOWN, KC_UP, MO(1),
+      KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_BSPC, KC_UP, MO(1),
       KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       ______, ______, ______, ______, ______, ______, ______, KC_PGUP, KC_PGDOWN, ______, ______, ______, ______, ______,
       ______, ______, ______, ______, ______, ______, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT, ______, ______, ______,
       ______, ______, ______, ______, ______, ______, KC_HOME, KC_END, ______, ______, ______, KC_MS_BTN3, ______, ______,
-      ______, ______, ______, ______, ______, ______, ______, ______, ______, ______, ______),
+      KC_RCTL, KC_RALT, ______, ______, ______, ______, ______, ______, ______, ______, ______),
 
 };
 
