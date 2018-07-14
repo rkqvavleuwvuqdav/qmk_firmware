c970f0aca95a1bf1cea36e0a6a2cae098c349e8c
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2018-07-13 18:13:10 -0700

    Well that typo was painful

--- keyboards/xd75/keymaps/quickhacks/keymap.c
+++ keyboards/xd75/keymaps/quickhacks/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | END    | INS    | PG DN  | N      | M      | ,      | .      | /      | RSHIFT |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LALT   | LGUI   | SPACE  | SPACE  | MO(1)  | LEFT   | DOWN   | RIGHT  | FN  | SPACE  | SPACE     | RGUI   | RALT   | RCTRL  |
+ * | LCTRL  | LALT   | LGUI   | SPACE  | SPACE  | MO(1)  | LEFT   | DOWN   | RIGHT  | MO(1)  | SPACE  | SPACE  | RGUI   | RALT   | RCTRL  |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS  },
   { KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, RESET,   _______, _______, _______  },
   { KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, _______, _______, _______, _______  },
-  { _______, _______, RGB_TOG, MO(_FN), RGB_RMOD,RGB_MOD, KC_P0,   _______, KC_PDOT, KC_PENT, KC_PENT, MO(_FN), _______, _______, _______  },
+  { _______, _______, RGB_TOG, MO(_FN), RGB_RMOD, _______, KC_P0,   _______, KC_PDOT, _______, _______, _______, _______, _______, _______  },
 },
 
  /* TG
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT  },
     { TG(2), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT,    KC_SCLN, KC_ENT   },
     { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_INS,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT  },
-    { KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT, MO(_FN), KC_SPC,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTL  },
+    { KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTL  },
 
   }
 
