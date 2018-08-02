f3202e0e9175fe96f8ec9d7ad9d2bc3aa31c112c
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2018-08-01 22:33:47 -0700

    so many mistakes

--- keyboards/xd75/keymaps/quickhacks/keymap.c
+++ keyboards/xd75/keymaps/quickhacks/keymap.c
@@ -84,9 +84,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_TG] = { /* QWERTY */
     { KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_MINS, KC_GRV,  KC_EQL,  KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC  },
     { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT  },
-    { TG(2), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT,    KC_SCLN, KC_ENT   },
+    { TG(2), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,    KC_SCLN, KC_ENT   },
     { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_INS,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT  },
-    { KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_SPC,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTL  },
+    { KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  _______,  _______, _______, KC_DOWN, _______, _______, _______,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTL  },
 
   },
 
