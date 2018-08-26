f1bee312f951fb78fbbb73a64005defde0f56d6b
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2018-08-26 12:43:15 -0700

    who need right control anyway?

--- keyboards/xd75/keymaps/quickhacks/keymap.c
+++ keyboards/xd75/keymaps/quickhacks/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------------------------+--------|
  * | LSHIFT | Z      | X      | C      | V      | B      | END    | INS    | PG DN  | N      | M      | ,      | .      | /      | ENTER  |
  * |--------+--------+--------+--------+--------+-----------------+--------+--------+--------+--------+-----------------+--------+--------|
- * | LCTRL  | LALT   | LGUI   | SPACE  | -      | MO(FN)  | LEFT   | DOWN   | RIGHT  | MO(UP)  | \      | SPACE  | RGUI   | RALT   | RCTRL  |
+ * | LCTRL  | LALT   | LGUI   | SPACE  | -      | MO(FN)  | LEFT   | DOWN   | RIGHT | MO(UP) | SPACE  | SPACE  | RGUI   | RALT   | \      |
  * '--------------------------------------------------------------------------------------------------------------------------------------'
  */
 
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC, KC_BSLS, KC_RBRC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT  },
   { TG(2), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_HOME, KC_DEL,  KC_PGUP, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_PIPE   },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_END,  KC_INS,   KC_PGDN, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT  },
-  { KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_MINS,  MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT, MO(_UP), KC_BSLS,  KC_SPC, KC_RGUI, KC_RALT, KC_RCTL  },
+  { KC_LCTL, KC_LALT, KC_LGUI, KC_SPC,  KC_MINS,  MO(_FN), KC_LEFT, KC_DOWN, KC_RGHT, MO(_UP), KC_SPC,  KC_SPC, KC_RGUI, KC_RALT, KC_BSLS  },
  },
 
 /* FUNCTION
