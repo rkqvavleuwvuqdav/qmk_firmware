36960a83eaec8abe7c88a1aa4c55b63da692a774
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2018-09-09 17:36:25 -0700

    well this makes more sense maybe

--- keyboards/dz60/keymaps/mine/keymap.c
+++ keyboards/dz60/keymaps/mine/keymap.c
@@ -24,16 +24,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-----------------------------------------------------------------------------------------+
  * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | INS |  U  | FN  |
  * |-----------------------------------------------------------------------------------------+
- * | Ctrl |  Alt  |  Cmd  |              Space                |  MO3   | BTN3 |  L  |  D  |  R  |
+ * | Ctrl |  Alt  |  Cmd  |              Space                |  MO4  | BTN3 |  L  |  D  |  R  |
  * `-----------------------------------------------------------------------------------------'
  */
 
   LAYOUT_2_shifts(
       KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, ______, KC_BSPC,
       KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
-      MO(2), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
+      MO(4), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
       KC_LSFT, ______, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_INS, KC_UP, MO(1),
-      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, MO(4), KC_BTN3, KC_LEFT, KC_DOWN, KC_RIGHT
+      KC_LCTL, KC_LALT, KC_LGUI, KC_SPC, KC_SPC, KC_SPC, MO(2), KC_BTN3, KC_LEFT, KC_DOWN, KC_RIGHT
       ),
 
 
