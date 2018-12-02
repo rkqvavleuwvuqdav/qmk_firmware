c451163cab7f6fe3b72210f914d2af25befed5e0
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2018-12-01 20:36:59 -0800

    too much vodka tonight

--- keyboards/fractal/keymaps/eas/keymap.c
+++ keyboards/fractal/keymaps/eas/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      | Ctrl | Alt  | CMD  | TGme |    Space    |TGlwr |  CMD | INS  |   -  |Enter |
+ * |MOrais| Ctrl | Alt  | CMD  | MOme |    Space    |TGlwr |  CMD | INS  |   -  |Enter |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_5x12( \
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,  \
-  _______, KC_LCTL, KC_LALT, KC_LGUI,   MO(_ME), KC_SPC,  KC_SPC,  TG(_LOWER),   KC_LGUI, KC_INSERT, KC_MINS,   KC_ENT  \
+  MO(_RAISE), KC_LCTL, KC_LALT, KC_LGUI,   MO(_ME), KC_SPC,  KC_SPC,  TG(_LOWER),   KC_LGUI, KC_INSERT, KC_MINS,   KC_ENT  \
 ),
 
 
