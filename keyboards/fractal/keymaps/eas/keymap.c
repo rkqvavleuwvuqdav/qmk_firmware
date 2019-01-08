30db9989bce10c66e39a4ab6cdc555bef2000201
John Doe <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2019-01-07 21:31:54 -0800

    mess cleanup

--- keyboards/fractal/keymaps/eas/keymap.c
+++ keyboards/fractal/keymaps/eas/keymap.c
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, \
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
   KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT)	,  \
+  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT)	,  \
   MO(_RAISE), KC_LCTL, KC_LALT, KC_LGUI,   MO(_ME), KC_SPC,  KC_SPC,  MO(_LOWER),   KC_LGUI, KC_RALT, KC_LCTL,   KC_ENT  \
 ),
 
@@ -74,9 +74,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ME] = LAYOUT_ortho_5x12( \
   KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    KC_DEL, \
-  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_F,    KC_LBRC,    KC_RBRC,    KC_MINS,    KC_EQL,    KC_PIPE,  \
+  KC_TAB,  KC_QUOT, KC_COMM, KC_DOT,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_PGUP,    KC_PGDN,    KC_INSERT,    KC_TRNS,    KC_PIPE,  \
   KC_ESC,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_LEFT,    KC_DOWN, KC_UP, KC_RIGHT,    KC_S,    KC_QUOT, \
-  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    _______,    _______,    _______,    _______,    _______,  \
+  KC_LSFT, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    KC_B,    KC_HOME,    KC_END,    _______,    _______,    _______,  \
   _______, _______, _______, _______,   _______, KC_TRNS,  KC_TRNS,  _______,   _______, _______, _______,   _______  \
 ),
 
