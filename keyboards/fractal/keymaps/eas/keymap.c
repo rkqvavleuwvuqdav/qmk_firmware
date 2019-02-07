b5de51359e748340442c1194e4d9561a8a50603f
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2019-02-06 20:59:26 -0800

    not sure if this is awesome or horrible

--- keyboards/fractal/keymaps/eas/keymap.c
+++ keyboards/fractal/keymaps/eas/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,  \
   MT(MOD_LCTL,KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT)	,  \
-  MO(_RAISE), KC_LCTL, KC_LALT, KC_LGUI,   MO(_ME), KC_SPC,  KC_SPC,  MO(_LOWER),   KC_LGUI, KC_RALT, KC_LCTL,   KC_ENT  \
+  MO(_RAISE), KC_LCTL, KC_LALT, KC_LGUI,   MO(_ME), KC_SPC,  LT(_RAISE,KC_SPC),  MO(_LOWER),   KC_LGUI, KC_RALT, KC_LCTL,   KC_ENT  \
 ),
 
 
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |RESET |      |   -  |   =  |   [  |   ]  |      |
+ * |      |      |      |      |      |      |      |   -  |   =  |   [  |   ]  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |ISO # |ISO / |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -117,7 +117,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_ortho_5x12( \
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,    KC_BSPC, \
   _______,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,  \
-  _______,  _______,   _______,   _______,   _______,   RESET,   _______,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
+  _______,  _______,   _______,   _______,   _______,    _______,   _______,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
   _______, _______,   _______,   _______,   _______,  _______,  _______,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, RESET  \
 ),
