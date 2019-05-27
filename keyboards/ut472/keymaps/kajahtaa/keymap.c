aa64c807ffa2ebdf3ca6c675d81c9d48e54aac6d
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2019-05-27 15:18:10 -0700

    tiny cleanup

--- keyboards/ut472/keymaps/kajahtaa/keymap.c
+++ keyboards/ut472/keymaps/kajahtaa/keymap.c
@@ -60,7 +60,7 @@ LAYOUT( /* Right */
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
   _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  _______,  KC_DOT,  _______,  _______,
-  _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, _______
+  _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
 ),
 
   /* FN Layer 2
@@ -79,7 +79,7 @@ LAYOUT( /* Left */
   KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-  _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, _______
+  _______, _______, _______, _______, _______,     _______,      _______, _______, _______, _______, _______
 ),
 
   /* FN Layer 2
