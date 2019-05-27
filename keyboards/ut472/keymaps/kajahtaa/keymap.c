fceabd2990a9ab6e4ac440baa3b378ffa70b4193
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2019-05-27 15:11:11 -0700

    test TG to num layer

--- keyboards/ut472/keymaps/kajahtaa/keymap.c
+++ keyboards/ut472/keymaps/kajahtaa/keymap.c
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |-------------------------------------------------------------------------+
    * | Shift |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  |Sh/En|
    * |-------------------------------------------------------------------------+
-   * | Mo1 |Cntrl| Alt | GUI |  L2  |   Space   |  L1  | Left| Down|  Up |Right|
+   * | Mo1 |Cntrl| Alt | GUI |  L2  |   Space   |  L1  | Left| Down|  Up |     |
    * `-------------------------------------------------------------------------'
    */
 
@@ -41,7 +41,7 @@ LAYOUT(
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-  MO(1),  KC_LCTL, KC_LALT, KC_LGUI, MO(3),      KC_SPC,        MO(2),  RCMD_LEFT, RALT_DOWN, RCTL_UP,   KC_RGHT
+  MO(1),  KC_LCTL, KC_LALT, KC_LGUI, MO(3),      KC_SPC,        MO(2),  RCMD_LEFT, RALT_DOWN, RCTL_UP,  TG(1)
 ),
 
   /* FN Layer 1
@@ -60,7 +60,7 @@ LAYOUT( /* Right */
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DELETE,
   _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
   _______, KC_F11,  KC_F12,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  _______,  KC_DOT,  _______,  _______,
-  _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, _______
 ),
 
   /* FN Layer 2
@@ -77,9 +77,9 @@ LAYOUT( /* Right */
 
 LAYOUT( /* Left */
   KC_TILDE,  KC_EXCLAIM,  KC_AT,  KC_HASH,  KC_DOLLAR, KC_PERCENT, KC_CIRCUMFLEX, KC_AMPERSAND, KC_ASTERISK, KC_LEFT_PAREN, KC_RIGHT_PAREN, KC_DELETE,
-  _______, _______, _______, _______, _______, _______, _______, KC_DOWN, KC_UP, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT_CURLY_BRACE, KC_RIGHT_CURLY_BRACE, KC_PIPE,
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  _______,
-  _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
+  _______, _______, _______, _______, _______,     _______,      _______, KC_HOME, KC_PGDN, KC_PGUP, _______
 ),
 
   /* FN Layer 2
@@ -98,7 +98,7 @@ LAYOUT( /* Tab */
   KC_ESC,  _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, KC_INSERT,  KC_PSCR, _______,
   _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______,  KC_PIPE,
   _______, _______, _______, _______, _______, _______, _______, KC_END, KC_HOME, _______, _______, _______,
-  _______, _______, _______, RESET, _______,     _______,      _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R
+  _______, _______, _______, RESET, _______,     _______,      _______, KC_MS_L, KC_MS_D, KC_MS_U, _______
 ),
 
 
