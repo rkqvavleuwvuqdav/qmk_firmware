b1b4e9bce4138fd92d017e150b2875a0acd8ef05
Not Your Name <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2019-10-13 21:50:10 -0700

    move right layer MO to -=[]

--- keyboards/xd75/keymaps/kajahtaa/keymap.c
+++ keyboards/xd75/keymaps/kajahtaa/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   { KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_7, KC_8, KC_9, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH  },
   { CTL_ESC, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_4, KC_5,  KC_6, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT   },
   { KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_1,  KC_2,   KC_3, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT)  },
-  { MO(_UP), KC_LCTL, KC_LALT, LGUI_T(KC_RIGHT),  MO(_TG),  KC_SPC, KC_0, KC_0, KC_SPC, KC_SPC, MO(_FN),   RCMD_LEFT, RALT_DOWN, RCTL_UP, _______  },
+  { MO(_UP), KC_LCTL, KC_LALT, LGUI_T(KC_RIGHT),  MO(_TG),  KC_SPC, KC_0, KC_0, KC_SPC, KC_SPC, MO(_UP),   RCMD_LEFT, RALT_DOWN, RCTL_UP, KC_RIGHT  },
  },
 
 /* FUNCTION
