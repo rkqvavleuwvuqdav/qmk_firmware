94d56934f8fec23158cec18242e1900f34fde23a
John Doe <37030722+rkqvavleuwvuqdav@users.noreply.github.com>
2020-03-28 13:01:38 -0700

    attempt space MO to number layer

--- keyboards/fortitude60/keymaps/kajahtaa/keymap.c
+++ keyboards/fortitude60/keymaps/kajahtaa/keymap.c
@@ -31,6 +31,7 @@ enum custom_keycodes {
 #define RALT_DOWN RALT_T(KC_DOWN)
 #define RCTL_UP RCTL_T(KC_UP)
 #define LCMD_RIGHT LGUI_T(KC_RIGHT)
+#define SPC_NUM LT(_karjalasta,KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -52,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLASH, \
   CTL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_QUOT, \
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, _______, _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT_RSFT_ENT , \
-                 KC_LALT, LCMD_RIGHT,   LOWER,  KC_ENT, KC_SPC,  KC_SPC, KC_SPC, RAISE,    RCMD_LEFT, RALT_DOWN\
+                 KC_LALT, LCMD_RIGHT,   LOWER,  KC_ENT, SPC_NUM,  SPC_NUM, KC_SPC, RAISE,    RCMD_LEFT, RALT_DOWN\
 ),
 
 /* Colemak
@@ -91,9 +92,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_karjalasta] = LAYOUT( \
   KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,                   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, \
-  KC_NO,  KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                   KC_F,    KC_G,    KC_C,    KC_R,    KC_L,    KC_BSPC, \
-  KC_NO,  KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                   KC_D,    KC_H,    KC_T,    KC_N,    KC_S,    KC_SLSH, \
-  KC_NO, KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X, KC_NO, KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO , \
+  KC_NO,  KC_NO, KC_NO, KC_NO,  KC_NO,    KC_NO,                   KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO, \
+  KC_NO,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_NO, \
+  KC_NO, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_NO, KC_NO, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,    KC_NO , \
                  KC_NO, KC_NO,   KC_NO,  KC_NO, KC_NO,  KC_NO, KC_NO, KC_NO,    KC_NO, KC_NO\
 ),
 
